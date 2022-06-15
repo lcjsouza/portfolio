96004269e7894d12320e66dbdcf37d8e73c27c1e
Luiz Junior <lcj_souza@yahoo.com.br>
2022-06-15 03:58:42 -0300

    Construção do menu, home, sobre e modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { ContatosComponent } from './contatos/contatos.component';
+import { FooterComponent } from './footer/footer.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    SobreComponent,
+    ExperienciasComponent,
+    HabilidadesComponent,
+    ContatosComponent,
+    FooterComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
